db3c72282c138df0e834424c7f8ffeb67368bc7d
libx266 <rozpodrawa@gmail.com>
2022-12-27 18:17:14 +0400

    ;

--- VkUtils/VkProcessor.cs
+++ VkUtils/VkProcessor.cs
@@ -19,7 +19,7 @@ namespace VkDialogParser.VkUtils
                 dynamic? response = null;
 
                 try { response = await vk.GetAsync("messages.getConversations", new() { ["count"] = 200 + "", ["offset"] = offset + "" }); }
-                catch (Exception ex) { ex.Log(); goto Step }
+                catch (Exception ex) { ex.Log(); goto Step; }
 
 
                 foreach (dynamic item in response.response.items)
