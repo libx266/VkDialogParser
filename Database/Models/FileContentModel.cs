2af8c0d1da71917c5c71b86567bdc7d06579e78a
libx266 <rozpodrawa@gmail.com>
2022-12-27 13:22:13 +0400

    fix self-reflected loop

--- Database/Models/FileContentModel.cs
+++ Database/Models/FileContentModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace VkDialogParser.Database.Models
 {
     public class FileContentModel : BaseModel
     {
+        [JsonIgnore]
         public MessageModel Message { get; set; } 
 
         public string FileName { get; set; }
